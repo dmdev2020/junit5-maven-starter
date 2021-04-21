3e8ffd6a28c5b00d76c0e1d342d3e794b5b9e6a5
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-04-21 09:33:24 +0300

    added files from lesson-2

---
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.dmdev.junit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserServiceTest {
+
+    @Test
+    void test() {
+        assertTrue(false);
+    }
+}
