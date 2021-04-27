3227f02f146b04b01df66bbacb63b8dc0e577412
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-04-27 10:10:31 +0300

    added files from lesson-4

--- src/main/java/com/dmdev/junit/service/UserService.java
+++ src/main/java/com/dmdev/junit/service/UserService.java
@@ -2,12 +2,18 @@ package com.dmdev.junit.service;
 
 import com.dmdev.junit.dto.User;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
 
+    private final List<User> users = new ArrayList<>();
+
     public List<User> getAll() {
-        return Collections.emptyList();
+        return users;
+    }
+
+    public boolean add(User user) {
+        return users.add(user);
     }
 }
--- src/test/java/com/dmdev/junit/service/UserServiceTest.java
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -1,16 +1,58 @@
 package com.dmdev.junit.service;
 
+import com.dmdev.junit.dto.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestInstance(Lifecycle.PER_CLASS)
 class UserServiceTest {
+
+    private UserService userService;
+
+    @BeforeAll
+    void init() {
+        System.out.println("Before all: " + this);
+    }
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("Before each: " + this);
+        userService = new UserService();
+    }
 
     @Test
     void usersEmptyIfNoUserAdded() {
-        var userService = new UserService();
+        System.out.println("Test 1: " + this);
         var users = userService.getAll();
         assertTrue(users.isEmpty(), () -> "User list should be empty");
 //        input -> [box == func] -> actual output
+    }
+
+    @Test
+    void usersSizeIfUserAdded() {
+        System.out.println("Test 2: " + this);
+        userService.add(new User());
+        userService.add(new User());
+
+        var users = userService.getAll();
+        assertEquals(2, users.size());
+    }
+
+    @AfterEach
+    void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
+    }
+
+    @AfterAll
+    void closeConnectionPool() {
+        System.out.println("After all: " + this);
     }
 }
